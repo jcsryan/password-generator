b9ec90da937db0fde5f46cc19433fa7a62baa6ea
JCSR <johnconnorsryan@gmail.com>
2020-04-05 08:18:40 -0400

    Readme updated, second and third logics put in 'other logics' file, gitpages tested.

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 # password-generator
 a random password generator
 
-The following JS logic pulls from four existing strings and concatenates them into a single larger string depending on the users choice. The logic then pulls, at random, from this larger string to create the password. I chose this way of doing the assignemnt because the code was the simplest and most easily understood of the three ways in which I got the generator to work. 
+The following JS logic pulls from four existing strings and concatenates them into a single larger string depending on the users choice. The logic then pulls, at random, from this larger string to create the password. I chose this version of the assignemnt because the code was the simplest and most easily understood of the three ways in which I got the generator to work. 
 
-The first and second versions of JS logic I created are in the file other logics, they both work when linked up to the HTML file.
+The first and second versions of JS logic I created are in the file 'other logics', they both work when linked up to the HTML file.
+These two logics use arrays and either an 'else if' or 'switch' function to choose which arrays to concatenate. I initally wrote the else if function and thought it was too large for such a simple program, making the possibility of later edits or additions harder than necessary. From there I tried to shorten that code by reorganizing the else if statements into a switch. While they both function they are each over sixty lines of code. 
+
+
 Here is the chosen logic. 
 
 ![](2020-04-05-08-05-21.png)
--- resources/script1.js
+++ resources/script1.js
@@ -1,3 +1,4 @@
+//create six strings, four to choose from, two to concatenate into.
 var alphabet = "abcdefghijklmnopqrstuvwxyz"
 var alphabetCap = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 var symbol = "*+-,./:<=>,?@[]^`{|}~"
@@ -5,6 +6,8 @@ var numbers = "1234567890"
 var givenString = ''
 var pwdString =''
 
+//generate a number from the user's choice in the initial click of 
+//the generate password button.
 var get = document.querySelector("#generate");
 
 get.addEventListener("click", function () {
@@ -12,14 +15,19 @@ get.addEventListener("click", function () {
     document.getElementById("password").placeholder = ps;
 });
 
+//function to add each string the user picks into an initally
+//empty string, if the user picks less than 8 or more than 124
+// the function generates a warning and restarts.
 function generatePassword() {
+//givenString and totalString are set to empty here so the function
+//can be rerun without refreshing the  page.
     givenString = ''
     totalString = ''
     enter = parseInt(prompt("How many characters would you like your password? Choose between 8 and 128"));
 
     if (!enter) {
         alert("This needs a value");
-    } else if (enter < 8 || enter > 128) {
+    } else if (enter < 8 || enter > 124) {
 
         enter = parseInt(prompt("You must choose between 8 and 128"));
 
@@ -39,6 +47,10 @@ function generatePassword() {
     }
     var totalString = givenString;
      
+//Simple for loop that generates a random password by taking the
+//length of the users password and randomly pulling from the
+//concatenated list which is set to totalString at the end of
+//the function above.    
 var passwordTotal=''
 for (var i = 0; i < enter; i++) {
     var pickChoices = totalString[Math.floor(Math.random() * totalString.length)];
